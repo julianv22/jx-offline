caac7a5bfb06f5c8e5eafdbd8211a4899cd84ecf
Julian-V <julianv2212@gmail.com>
2023-09-24 16:21:25 +0700

    Hồi thiên tái tạo lễ bao

--- script/global/gm/julianv/lib/lib_vatpham.lua
+++ script/global/gm/julianv/lib/lib_vatpham.lua
@@ -1,5 +1,4 @@
 tbNguyenLieu = {}
-tbVatPham = {}
 tbNguyenLieu.tbList = {
     HuyenTinh = 1,
     ThuyTinh = 2,
@@ -74,7 +73,42 @@ tbNguyenLieu.tbVatPham = {
         tbParam = {60}
     }}
 }
-
+tbVatPham = {}
+tbVatPham.Khac = {{
+    szName = "Håi Thiªn T¸i T¹o LÔ Bao",
+    tbProp = {6, 1, 1781, 0, 0, 0},
+    tbParam = {60}
+}, {
+    szName = "Khiªu ChiÕn LÖnh.",
+    tbProp = {6, 1, 1499, 0, 0, 0}
+}, {
+    szName = "Cµn Kh«n Song TuyÖt Béi",
+    tbProp = {6, 1, 2219, 0, 0, 0}
+}, {
+    szName = "Nh¹c V­¬ng KiÕm LÔ Bao",
+    tbProp = {6, 1, 2340, 0, 0, 0}
+}, {
+    szName = "Long HuyÕt Hoµng",
+    tbProp = {6, 1, 2340, 0, 0, 0}
+}, {
+    szName = "Tö MÉu LÖnh",
+    tbProp = {6, 1, 1427, 0, 0, 0}
+}, {
+    szName = "TÝn VËt M«n Ph¸i",
+    tbProp = {6, 1, 1670, 0, 0, 0}
+}, {
+    szName = "TÝn VËt D­¬ng Anh",
+    tbProp = {6, 1, 1671, 0, 0, 0}
+}, {
+    szName = "Hçn Nguyªn Linh Lé",
+    tbProp = {6, 1, 2312, 1, 0, 0}
+}, {
+    szName = "§¹i Lùc Hoµn LÔ Hép",
+    tbProp = {6, 1, 2517, 0, 0, 0}
+}, {
+    szName = "ThËp Toµn §¹i Bæ",
+    tbProp = {6, 1, 1399, 0, 0, 0}
+}}
 function tbNguyenLieu:Define()
     for id, VatPham in tbNguyenLieu.tbVatPham do
         tbVatPham[id] = VatPham

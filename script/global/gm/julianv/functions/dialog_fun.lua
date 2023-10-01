f3d1c9892553f6d569feb76f2b3a4737bc10281f
Julian-V <julianv2212@gmail.com>
2023-10-01 19:40:43 +0700

    update skills.txt

--- script/global/gm/julianv/functions/dialog_fun.lua
+++ script/global/gm/julianv/functions/dialog_fun.lua
@@ -47,7 +47,7 @@ function JDialog:DefaultTitle() -- Reset tieu de Dialog ve mac dinh
     local szCurCamp = format("<color=%s>%s<color>", tbMonPhai.tbPK_Status[GetCamp()][2],
                           tbMonPhai.tbPK_Status[GetCamp()][1])
     local szMessage = format(
-                          "<pic=137> Tµi kho¶n : <bclr=red>%s<bclr>     Index : <color=yellow>%s<color>\n<pic=136> Nh©n vËt  : <bclr=blue>%s<bclr>\n<pic=135> M«n ph¸i  : <color=green>%s<color>     CÊp ®é : <color=green>%d<color>\n<pic=136> Mµu PK    : %s   Trïng sinh : <color=green>%d<color>\n\n<pic=135> Online    : <color=green>%d<color>\n<pic=54> Täa ®é   : <color=yellow>%d,<color> <color=orange>%d/%d<color>",
+                          "<pic=137> Tµi kho¶n : <bclr=red>%s<bclr>     Index : <color=yellow>%s<color>\n<pic=136> Nh©n vËt  : <bclr=blue>%s<bclr>\n<pic=135> M«n ph¸i  : <color=green>%s<color>     CÊp ®é : <color=green>%d<color>\n<pic=136> ThÕ lùc   : %s   Trïng sinh : <color=green>%d<color>\n\n<pic=135> Online    : <color=green>%d<color>\n<pic=54> Täa ®é   : <color=yellow>%d,<color> <color=orange>%d/%d<color>",
                           szAccount, PlayerIndex, szName, szFactionName, GetLevel(), szCurCamp,
                           ST_GetTransLifeCount(), nOnline, nW, nX, nY)
     self:InitTitle(szMessage)

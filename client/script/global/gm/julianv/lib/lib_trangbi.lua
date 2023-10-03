5113b1a508452b82a2f4046698c3141add9ffdc7
Julian-V <julianv2212@gmail.com>
2023-10-03 22:43:54 +0700

    Update Danh Hiệu

--- client/script/global/gm/julianv/lib/lib_trangbi.lua
+++ client/script/global/gm/julianv/lib/lib_trangbi.lua
@@ -80,9 +80,10 @@ tbDanhHieu = {
         { "Long ThÇn KiÕm", 80 }, { "Thiªn H¹ §Ö NhÊt Bang", 189 },
     },
     KiemThe = {
-        { "Uy ChÊn Cöu Ch©u", 192 }, { "Vâ L©m ChÝ T«n", 193 },
+        { "GM Julian-V", 200 }, { "Uy ChÊn Cöu Ch©u", 192 }, { "Vâ L©m ChÝ T«n", 193 },
         { "NhÊt KiÕm Phong Thiªn", 194 }, { "NhÊt L­u Cao Thñ", 195 },
         { "HiÖp Kh¸ch Cuèi Cïng Trªn Giang Hå", 196 }, { "Phong V©n", 197 },
+        { "§¹i Th¸nh", 198 }, { "TrÊn Thiªn", 199 },
     },
 }
 

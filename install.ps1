8031f792d4d3cc470f05b0cb4f8abbfd71e16739
julianv22 <julianv2212@gmail.com>
2023-11-01 18:27:04 +0700

    install.ps1

--- install.ps1
+++ install.ps1
@@ -12,48 +12,63 @@ Start-Process -Wait powershell -verb runas -ArgumentList "Set-ItemProperty -Path
 
 Write-Start -msg "Installing Scoop..."
 if (Get-Command scoop -errorAction SilentlyContinue) {
-	Write-Warning "Scoop already installed"
+	Write-Warning "Scoop is already installed"
 } else {
 	#Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 	iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 }
+	Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
+	Import-Module Terminal-Icons
+if (!(test-path $PROFILE)) {
+	New-Item -Path $PROFILE -Type File -Force
+}
+	Clear-Content $PROFILE
+	Add-Content $PROFILE -Value "oh-my-posh init pwsh --config 'C:\Users\Admin\scoop\apps\oh-my-posh\current\themes\quick-term.omp.json' | Invoke-Expression
+Import-Module Terminal-Icons"
+	.$PROFILE
 Write-Done
 
-Write-Start -msg "Installing Git..."
-	scoop install git	
+Write-Start -msg "Installing git & buckets..."
+	scoop install git    
 	scoop bucket add extras
 	scoop bucket add nerd-fonts
 	scoop bucket add java
     scoop bucket add nonportable
+    scoop bucket add versions    
 	scoop update
 Write-Done
 
-Write-Start -msg "Installing Scoop's packages"
-	# scoop install <# Browsers #> firefox googleschrome
-	scoop install <# Sofwares #>			obs-studio wpsoffice winscp
-	scoop install <# Developing #>			vscode nodejs python notepadplusplus hxd
-	scoop install <# Community #>			discord
-    # scoop install <# Virtual Machine #>		nonportable/virtualbox-np
-	Start-Process -Wait powershell -verb runas -ArgumentList "scoop install extras/ida-free"
+Write-Start -msg "Installing Scoop's packages..."
+	scoop install <# Nerd Fonts #>			Hack-NF windows-terminal oh-my-posh
+	scoop install <# Developing #>			vscode nodejs python notepadplusplus hxd winscp	
+	scoop install <# Sofwares #>			obs-studio discord wpsoffice	
+	scoop install <# Games #>				steam	
+    #scoop install <# Virtual Machine #>		nonportable/virtualbox-np
+	Start-Process -Wait powershell -verb runas -ArgumentList "scoop install ida-free"
 	Start-Process -Wait powershell -verb runas -ArgumentList "scoop install DejaVuSansMono-NF-Mono vcredist-aio"
 Write-Done
 
-Write-Start -msg "Configuring VSCode"
-	code --install-extension adpyke.codesnap --force
-	code --install-extension visualstudioexptteam.vscodeintellicode --force	
-	code --install-extension esbenp.prettier-vscode --force
-	code --install-extension vscode-icons-team.vscode-icons --force
-	code --install-extension dbaeumer.vscode-eslint --force
-	code --install-extension visualstudioexptteam.intellicode-api-usage-examples --force
-	code --install-extension chakrounanas.turbo-console-log --force
+Write-Start -msg "Installing extensions..."		
+	code --install-extension vscode-icons-team.vscode-icons --force	
 Write-Done
 
-Write-Start -msg "Enable Viertualization"
+Write-Start -msg "Enable Virtualization"
 	Start-Process -Wait powershell -verb runas -ArgumentList "echo y | Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -Norestart"
 	Start-Process -Wait powershell -verb runas -ArgumentList "echo y | Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -Norestart"
 	Start-Process -Wait powershell -verb runas -ArgumentList " echo y | Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All"
 	Start-Process -Wait powershell -verb runas -ArgumentList "echo y | Enable-WindowsOptionalFeature -Online -FeatureName Containers -All"
 Write-Done
 
+Write-Start -msg "Cleanup apps by removing old verions"
+    scoop cleanup -a
+Write-Done
+
+Write-Start -msg "Clear the download cache"
+    scoop cache rm -a
+Write-Done
+
+Write-Start -msg "Show status and check for new app versions"
     scoop status
+Write-Done
+	windowsterminal.exe
 #END

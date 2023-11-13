962381474805f5e7a334d352a19f795ec08d70cc
julianv22 <julianv2212@gmail.com>
2023-11-13 18:33:26 +0700

    install.ps1

--- install.ps1
+++ install.ps1
@@ -18,9 +18,9 @@ function Write-Done { Write-Host "DONE" -ForegroundColor Yellow; Write-Host }
 Write-Start "Installing scoop..."
 	if (Get-Command scoop -errorAction SilentlyContinue) {
 		Write-Warning "Scoop is already installed"
-	} else {		
+	} else {
 		iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
-		# irm get.scoop.sh | iex		
+		# irm get.scoop.sh | iex
 	}
 Write-Done
 
@@ -36,11 +36,11 @@ Write-Start "Installing git & buckets..."
 Write-Done
 
 Write-Start "Installing Windows Terminal & Powershell..."
-	scoop install windows-terminal pwsh psreadline oh-my-posh terminal-icons	
+	scoop install windows-terminal pwsh psreadline oh-my-posh terminal-icons
 	Import-Module Terminal-Icons
 	Import-Module PSReadLine
 	Set-PSReadLineOption -PredictionSource History
-	Set-PSReadLineOption -PredictionViewStyle ListView	
+	Set-PSReadLineOption -PredictionViewStyle ListView
 	Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 	if (!(test-path $PROFILE)) {
 		New-Item -Path $PROFILE -Type File -Force
@@ -52,11 +52,11 @@ Write-Done
 
 Write-Start "Installing apps..."
 	Write-App "<# Fonts #>";		scoop install DejaVuSansMono-NF-Mono
-	Write-App "<# Developing #>";	scoop install nodejs python vscode notepadplusplus
+	Write-App "<# Developing #>";	scoop install nodejs python vscode kate
 	Write-App "<# Sofwares #>";		scoop install obs-studio hibit-uninstaller vcredist-aio # discord wpsoffice bleachbit
 	Write-App "<# Games #>";		scoop install steam
 	Write-App "<# JX-Tools #>";		scoop install winscp hxd ida-free
-	Write-App "<# Apps #>";			scoop install neofetch btop nano irfanview nilesoft-shell shutup10 kdeconnect winget    
+	Write-App "<# Apps #>";			scoop install neofetch btop nano irfanview nilesoft-shell shutup10 kdeconnect winget
 	Write-Host; code --install-extension vscode-icons-team.vscode-icons --force
 	Write-App "Add 'Open with Code' to Context Menu" # Add 'Open with Code' to Context Menu
 	reg import "C:\Users\Admin\scoop\apps\vscode\current\install-context.reg"
